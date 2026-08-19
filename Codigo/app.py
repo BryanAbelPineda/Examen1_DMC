7e95cb77258f906c0b9d273d9038968d2cd8fbbe
BryanAbelPineda <127690769+BryanAbelPineda@users.noreply.github.com>
2026-08-19 00:20:02 -0500

    Update app.py

--- Codigo/app.py
+++ Codigo/app.py
@@ -1,2 +1,2 @@
 import streamlit as st
-modulos = st.sidebar.selectbox ("Selecione un módulo", ["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
+modulos = st.sidebar.selectbox ("Índice", ["Home", "Ejercicio 1", "Ejercicio 2","Ejercicio 3","Ejercicio 4"])
