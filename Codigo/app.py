7072397b37e462054459793f2ecdcf17f921fa0f
BryanAbelPineda <127690769+BryanAbelPineda@users.noreply.github.com>
2026-08-19 00:59:00 -0500

    Update app.py

--- Codigo/app.py
+++ Codigo/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.title("Trabajo Práctico - Modulo Python Fundamentals")
-st.sidebar.title("Parámetros")
-st.write("Elaborado por: Bryan Abel Pineda Sulca")
 st.image("Logos/Python_Logo.png", width = 100)
+st.title("Trabajo Práctico - Modulo Python Fundamentals")
+st.sidebar.title("Menú")
+st.write("Elaborado por: Bryan Abel Pineda Sulca")
 st.sidebar.image("Logos/DMC_logo.png",width = 200)
 modulos = st.sidebar.selectbox ("Secciones", ["Home", "Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 
