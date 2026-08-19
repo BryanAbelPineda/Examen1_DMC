2b2ac02907e501e8e61e61c7cca230a60a0bd488
BryanAbelPineda <127690769+BryanAbelPineda@users.noreply.github.com>
2026-08-19 00:57:34 -0500

    Update app.py

--- Codigo/app.py
+++ Codigo/app.py
@@ -4,6 +4,6 @@ st.title("Trabajo Práctico - Modulo Python Fundamentals")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Bryan Abel Pineda Sulca")
 st.image("Logos/Python_Logo.png", width = 100)
-st.sidebar.image("Logos/DMC_logo.png",width = 100)
+st.sidebar.image("Logos/DMC_logo.png",width = 200)
 modulos = st.sidebar.selectbox ("Secciones", ["Home", "Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 
