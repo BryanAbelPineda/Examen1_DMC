1334bb575006868632655d308f4e836c9136bf40
BryanAbelPineda <127690769+BryanAbelPineda@users.noreply.github.com>
2026-08-22 01:34:59 -0500

    Update app.py

--- Codigo/app.py
+++ Codigo/app.py
@@ -34,9 +34,9 @@ if modulos == "Home":
     """)
 elif modulos == "Ejercicio 1":
   st.write("1. FLUJO DE CAJA CON LISTAS")
-  nombre = st.text_input("Ingrese su nombre:")
-  if nombre:
-    st.write("Hola", nombre)
+  nombre = st.text_input("Ingrese Concepto:")
+  tipo_movimiento = st.selectbox("Tipo de Movimiento",["Ingreso","Gasto"])
+
 else:
 
   
