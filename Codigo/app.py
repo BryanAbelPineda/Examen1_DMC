f5b57af9ce7e9c6e52213d03827c6b5fa13f58c9
BryanAbelPineda <127690769+BryanAbelPineda@users.noreply.github.com>
2026-08-23 12:56:32 -0500

    Update app.py

--- Codigo/app.py
+++ Codigo/app.py
@@ -160,13 +160,11 @@ elif modulos == "Ejercicio 3":
 #################################################################################
 
       if st.button("Calcular Productividad"):
-           calcular_productividad_laboral(up,ht,nt)
-      
-      ahora=datetime.now()
-      st.metric(label="Productividad por hora",value=productividad_hora)
-      st.metric(label="Productividad por Trabajador",value=productividad_trabajador)
+           resultado=calcular_productividad_laboral(up,ht,nt)  
+           st.metric(label="Productividad por hora",value=productividad_hora)
+           st.metric(label="Productividad por Trabajador",value=productividad_trabajador)
       st.write("Hora Actual",ahora)
-
+      ahora=datetime.now()
 else:
 
   
